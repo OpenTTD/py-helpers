5249474d31e262ee343bfc1b7a3ac88abe01e3cf
Patric Stout <truebrain@openttd.org>
2022-08-10 18:34:30 +0200

    Change: also add the logger name to the logs
    Often I notice that the log alone doesn't really help out in to
    narrow down where the issue is. Adding the logger name alone
    already helps a lot.

--- openttd_helpers/logging_helper/click_logging.py
+++ openttd_helpers/logging_helper/click_logging.py
@@ -12,5 +12,5 @@ def click_logging():
     """
 
     logging.basicConfig(
-        format="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO
+        format="%(asctime)s %(levelname)-8s [%(name)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO
     )
